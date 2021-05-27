b41b4b875f6e430949de88bafd2b1b87db4d0e51
Miguel <cimildoro@gmail.com>
2021-05-27 18:27:45 +0200

    añadiendo bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+var nodo;
+
+
+
+function remove(){
+    let nodo = document.getElementsByClassName("position");
+    let img = document.getElementsByClassName("lex-img_header");
+    nodo.removeChild(img);
+    
+
+
+}
